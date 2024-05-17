d4f79486c61cf2920d357b48f2e39910c8391303
niblit <niblit.gh@gmail.com>
2024-05-17 04:01:56 -0600

    Replaced ToString trait to Display Added as_player function Added tests

--- logic/src/components/team.rs
+++ logic/src/components/team.rs
@@ -1,13 +1,47 @@
+use std::fmt;
+
 pub enum Team {
-    White,
     Black,
+    White,
 }
 
-impl ToString for Team {
-    fn to_string(&self) -> String {
+const BLACK_SHORT: &str = "b";
+const WHITE_SHORT: &str = "w";
+
+const BLACK_LONG: &str = "Black";
+const WHITE_LONG: &str = "White";
+
+impl Team {
+    pub fn as_player(&self) -> &str {
         match self {
-            Team::White => String::from("w"),
-            Team::Black => String::from("b"),
+            Team::Black => BLACK_LONG,
+            Team::White => WHITE_LONG,
         }
+    }
+}
+
+impl fmt::Display for Team {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Team::Black => write!(f, "{}", BLACK_SHORT),
+            Team::White => write!(f, "{}", WHITE_SHORT),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_team_as_player() {
+        assert_eq!(Team::Black.as_player(), BLACK_LONG);
+        assert_eq!(Team::White.as_player(), WHITE_LONG);
+    }
+
+    #[test]
+    fn test_team_display() {
+        assert_eq!(format!("{}", Team::Black), BLACK_SHORT);
+        assert_eq!(format!("{}", Team::White), WHITE_SHORT);
     }
 }

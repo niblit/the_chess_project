1dcd83876e4f4fdc4ba41c5caac18a1d54df916e
niblit <niblit.gh@gmail.com>
2024-06-03 02:44:33 -0600

    Added test cases

--- logic/src/components/piece.rs
+++ logic/src/components/piece.rs
@@ -17,3 +17,47 @@ impl fmt::Display for Piece {
         write!(f, "{}{}", self.color, self.kind)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::{PieceType, Team};
+
+    #[test]
+    fn as_sentence() {
+        for color in [Team::White, Team::Black] {
+            for kind in [
+                PieceType::Pawn,
+                PieceType::Knight,
+                PieceType::Bishop,
+                PieceType::Rook,
+                PieceType::Queen,
+                PieceType::King,
+            ] {
+                let piece = Piece { kind, color };
+                assert_eq!(piece.as_sentence(),
+                    format!("{} {}", color.as_sentence(), kind.as_sentence())
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn display() {
+        for color in [Team::White, Team::Black] {
+            for kind in [
+                PieceType::Pawn,
+                PieceType::Knight,
+                PieceType::Bishop,
+                PieceType::Rook,
+                PieceType::Queen,
+                PieceType::King,
+            ] {
+                let piece = Piece { kind, color };
+                assert_eq!(piece.to_string(),
+                    format!("{}{}", color, kind)
+                );
+            }
+        }
+    }
+}

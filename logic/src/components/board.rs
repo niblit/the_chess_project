e68be975e820b0fda0e1614dd979cfa4cdac0877
niblit <niblit.gh@gmail.com>
2024-06-19 04:50:50 -0600

    Idk just include in mod.rs

---
+++ logic/src/components/board.rs
@@ -0,0 +1,7 @@
+use super::piece::Piece;
+
+pub struct Board {
+    grid: [[Piece; 8]; 8],
+}
+
+impl Board {}
--- logic/src/components/mod.rs
+++ logic/src/components/mod.rs
@@ -1,8 +1,8 @@
+mod board;
 mod castling_rights;
+mod coordinates;
 mod move_counter;
 mod outcome;
 mod piece;
 mod piece_type;
 mod team;
-mod board;
-mod coordinates;

b1b782bec9247e750b0eb99bd2c1989698510fd4
niblit <niblit.gh@gmail.com>
2024-09-20 18:29:06 -0600

    Removed unnecessary tests Added missing outcomes

--- logic/src/components/outcome.rs
+++ logic/src/components/outcome.rs
@@ -7,10 +7,17 @@ pub enum Outcome {
     Checkmate(Team),
     Resignation(Team),
     Timeout(Team),
+
     Stalemate,
+
     InsufficientMaterial,
+
     FiftyMoveRule,
+    EnforcedSeventyFiveMoveRule,
+
     ThreefoldRepetition,
+    EnforcedFivefoldRepetition,
+
     DrawByAgreement,
 }
 
@@ -19,9 +26,15 @@ const RESIGNATION: &str = "wins, opponent resigned";
 const TIMEOUT: &str = "wins by timeout";
 
 const STALEMATE: &str = "Draw by stalemate";
+
 const INSUFFICIENT_MATERIAL: &str = "Draw by insufficient material";
+
 const FIFTY_MOVE_RULE: &str = "Draw by fifty-move rule";
-const THREEFOLD_REPETITION: &str = "Draw by repetition";
+const SEVENTY_FIVE_MOVE_RULE: &str = "Draw by seventy-five-move rule";
+
+const THREEFOLD_REPETITION: &str = "Draw by threefold repetition";
+const FIVEFOLD_REPETITION: &str = "Draw by fivefold repetition";
+
 const BY_AGREEMENT: &str = "Draw by agreement";
 
 impl Outcome {
@@ -30,10 +43,17 @@ impl Outcome {
             Outcome::Checkmate(team) => format!("{} {}", team.as_sentence(), CHECKMATE),
             Outcome::Resignation(team) => format!("{} {}", team.as_sentence(), RESIGNATION),
             Outcome::Timeout(team) => format!("{} {}", team.as_sentence(), TIMEOUT),
+
             Outcome::Stalemate => String::from(STALEMATE),
+
             Outcome::InsufficientMaterial => String::from(INSUFFICIENT_MATERIAL),
+
             Outcome::FiftyMoveRule => String::from(FIFTY_MOVE_RULE),
+            Outcome::EnforcedSeventyFiveMoveRule => String::from(SEVENTY_FIVE_MOVE_RULE),
+
             Outcome::ThreefoldRepetition => String::from(THREEFOLD_REPETITION),
+            Outcome::EnforcedFivefoldRepetition => String::from(FIVEFOLD_REPETITION),
+
             Outcome::DrawByAgreement => String::from(BY_AGREEMENT),
         }
     }
@@ -61,75 +81,5 @@ impl fmt::Display for Outcome {
             _ => DRAW_SHORT,
         };
         write!(f, "{}", result_string)
-    }
-}
-
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_as_sentence() {
-        assert_eq!(
-            Outcome::Checkmate(Team::Black).as_sentence(),
-            format!("{} {}", Team::Black.as_sentence(), CHECKMATE)
-        );
-        assert_eq!(
-            Outcome::Checkmate(Team::White).as_sentence(),
-            format!("{} {}", Team::White.as_sentence(), CHECKMATE)
-        );
-
-        assert_eq!(
-            Outcome::Resignation(Team::White).as_sentence(),
-            format!("{} {}", Team::White.as_sentence(), RESIGNATION)
-        );
-        assert_eq!(
-            Outcome::Resignation(Team::Black).as_sentence(),
-            format!("{} {}", Team::Black.as_sentence(), RESIGNATION)
-        );
-
-        assert_eq!(
-            Outcome::Timeout(Team::White).as_sentence(),
-            format!("{} {}", Team::White.as_sentence(), TIMEOUT)
-        );
-        assert_eq!(
-            Outcome::Timeout(Team::Black).as_sentence(),
-            format!("{} {}", Team::Black.as_sentence(), TIMEOUT)
-        );
-
-        assert_eq!(Outcome::Stalemate.as_sentence(), STALEMATE);
-        assert_eq!(
-            Outcome::InsufficientMaterial.as_sentence(),
-            INSUFFICIENT_MATERIAL
-        );
-        assert_eq!(Outcome::FiftyMoveRule.as_sentence(), FIFTY_MOVE_RULE);
-        assert_eq!(
-            Outcome::ThreefoldRepetition.as_sentence(),
-            THREEFOLD_REPETITION
-        );
-        assert_eq!(Outcome::DrawByAgreement.as_sentence(), BY_AGREEMENT);
-    }
-
-    #[test]
-    fn test_fmt_display() {
-        assert_eq!(format!("{}", Outcome::Checkmate(Team::Black)), BLACK_SHORT);
-        assert_eq!(format!("{}", Outcome::Checkmate(Team::White)), WHITE_SHORT);
-
-        assert_eq!(
-            format!("{}", Outcome::Resignation(Team::Black)),
-            BLACK_SHORT
-        );
-        assert_eq!(
-            format!("{}", Outcome::Resignation(Team::White)),
-            WHITE_SHORT
-        );
-
-        assert_eq!(format!("{}", Outcome::Timeout(Team::Black)), BLACK_SHORT);
-        assert_eq!(format!("{}", Outcome::Timeout(Team::White)), WHITE_SHORT);
-
-        assert_eq!(format!("{}", Outcome::Stalemate), DRAW_SHORT);
-        assert_eq!(format!("{}", Outcome::InsufficientMaterial), DRAW_SHORT);
-        assert_eq!(format!("{}", Outcome::FiftyMoveRule), DRAW_SHORT);
-        assert_eq!(format!("{}", Outcome::ThreefoldRepetition), DRAW_SHORT);
-        assert_eq!(format!("{}", Outcome::DrawByAgreement), DRAW_SHORT);
     }
 }

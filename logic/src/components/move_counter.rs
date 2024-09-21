8b99e8185b58fb1dc52c97c07a8c8d1beb991786
niblit <niblit.gh@gmail.com>
2024-09-20 18:28:21 -0600

    Renamed member variables

--- logic/src/components/move_counter.rs
+++ logic/src/components/move_counter.rs
@@ -1,29 +1,33 @@
 pub struct MoveCounter {
-    halfmove: u128,
-    irreversible_move: Vec<u128>,
+    halfmoves: u128,
+    irreversible_moves: Vec<u128>,
 }
 
 impl MoveCounter {
     pub fn new() -> Self {
         Self {
-            halfmove: 0,
-            irreversible_move: vec![0],
+            halfmoves: 0,
+            irreversible_moves: vec![0],
         }
     }
 
     pub fn custom_start(halfmove: u128, irreversible_move: u128) -> Self {
         Self {
-            halfmove,
-            irreversible_move: vec![irreversible_move],
+            halfmoves: halfmove,
+            irreversible_moves: vec![irreversible_move],
         }
     }
 
     pub fn get_halfmove(&self) -> u128 {
-        self.halfmove
+        self.halfmoves
     }
 
     pub fn is_fifty_move_rule(&self) -> bool {
         self.get_last_irreversible_move() >= 100
+    }
+
+    pub fn is_seventy_five_move_rule(&self) -> bool {
+        self.get_last_irreversible_move() >= 150
     }
 
     pub fn tick(&mut self, was_reversible_move_made: bool) {
@@ -41,33 +45,33 @@ impl MoveCounter {
     }
 
     fn increment_halfmove(&mut self) {
-        self.halfmove = self.halfmove.saturating_add(1);
+        self.halfmoves = self.halfmoves.saturating_add(1);
     }
 
     fn decrement_halfmove(&mut self) {
-        self.halfmove = self.halfmove.saturating_sub(1);
+        self.halfmoves = self.halfmoves.saturating_sub(1);
     }
 
     fn reversible_move_made(&mut self) {
-        self.irreversible_move.push(0);
+        self.irreversible_moves.push(0);
     }
 
     fn increment_irreversible_move(&mut self) {
-        let last = self.irreversible_move.last_mut().unwrap();
+        let last = self.irreversible_moves.last_mut().unwrap();
         *last = last.saturating_add(1);
     }
 
     fn decrement_irreversible_move(&mut self) {
         if self.get_last_irreversible_move() > 0 {
-            let last = self.irreversible_move.last_mut().unwrap();
+            let last = self.irreversible_moves.last_mut().unwrap();
             *last = last.saturating_sub(1);
-        } else if self.get_last_irreversible_move() == 0 && self.irreversible_move.len() > 1 {
-            self.irreversible_move.pop();
+        } else if self.get_last_irreversible_move() == 0 && self.irreversible_moves.len() > 1 {
+            self.irreversible_moves.pop();
         }
     }
 
     fn get_last_irreversible_move(&self) -> u128 {
-        *self.irreversible_move.last().unwrap()
+        *self.irreversible_moves.last().unwrap()
     }
 }
 
@@ -78,24 +82,24 @@ mod tests {
     #[test]
     fn new() {
         let counter = MoveCounter::new();
-        assert_eq!(counter.halfmove, 0);
-        assert_eq!(counter.irreversible_move, vec![0]);
+        assert_eq!(counter.halfmoves, 0);
+        assert_eq!(counter.irreversible_moves, vec![0]);
     }
 
     #[test]
     fn custom_start() {
         let counter = MoveCounter::custom_start(3, 1416);
-        assert_eq!(counter.halfmove, 3);
-        assert_eq!(counter.irreversible_move, vec![1416]);
+        assert_eq!(counter.halfmoves, 3);
+        assert_eq!(counter.irreversible_moves, vec![1416]);
     }
 
     #[test]
     fn get_halfmove() {
         let counter = MoveCounter::new();
-        assert_eq!(counter.get_halfmove(), counter.halfmove);
+        assert_eq!(counter.get_halfmove(), counter.halfmoves);
 
         let counter = MoveCounter::custom_start(45, 294);
-        assert_eq!(counter.get_halfmove(), counter.halfmove);
+        assert_eq!(counter.get_halfmove(), counter.halfmoves);
     }
 
     #[test]
@@ -126,23 +130,23 @@ mod tests {
 
         counter.tick(false);
 
-        assert_eq!(counter.halfmove, 21);
-        assert_eq!(counter.irreversible_move, vec![16, 0, 0, 0, 1]);
+        assert_eq!(counter.halfmoves, 21);
+        assert_eq!(counter.irreversible_moves, vec![16, 0, 0, 0, 1]);
     }
 
     #[test]
     fn untick() {
         let mut counter = MoveCounter {
-            halfmove: 32,
-            irreversible_move: vec![16, 4, 6, 6],
+            halfmoves: 32,
+            irreversible_moves: vec![16, 4, 6, 6],
         };
 
         for _ in 0..16 {
             counter.untick();
         }
 
-        assert_eq!(counter.halfmove, 16);
-        assert_eq!(counter.irreversible_move, vec![16, 2]);
+        assert_eq!(counter.halfmoves, 16);
+        assert_eq!(counter.irreversible_moves, vec![16, 2]);
     }
 
     #[test]
@@ -152,27 +156,27 @@ mod tests {
             counter.increment_halfmove();
         }
 
-        assert_eq!(counter.halfmove, 64);
+        assert_eq!(counter.halfmoves, 64);
     }
 
     #[test]
     fn decrement_halfmove() {
         let mut counter = MoveCounter {
-            halfmove: 64,
-            irreversible_move: vec![0],
+            halfmoves: 64,
+            irreversible_moves: vec![0],
         };
 
         for _ in 0..32 {
             counter.decrement_halfmove();
         }
 
-        assert_eq!(counter.halfmove, 32);
+        assert_eq!(counter.halfmoves, 32);
 
         for _ in 0..256 {
             counter.decrement_halfmove();
         }
 
-        assert_eq!(counter.halfmove, 0);
+        assert_eq!(counter.halfmoves, 0);
     }
 
     #[test]
@@ -195,7 +199,7 @@ mod tests {
             counter.increment_irreversible_move();
         }
 
-        assert_eq!(counter.irreversible_move, vec![16, 0, 32, 64]);
+        assert_eq!(counter.irreversible_moves, vec![16, 0, 32, 64]);
     }
 
     #[test]
@@ -205,45 +209,45 @@ mod tests {
             counter.increment_irreversible_move();
         }
 
-        assert_eq!(counter.irreversible_move, vec![64]);
+        assert_eq!(counter.irreversible_moves, vec![64]);
     }
 
     #[test]
     fn decrement_irreversible_move() {
         let mut counter = MoveCounter {
-            halfmove: 64,
-            irreversible_move: vec![12, 0, 0, 234, 0, 32, 2],
+            halfmoves: 64,
+            irreversible_moves: vec![12, 0, 0, 234, 0, 32, 2],
         };
 
         for _ in 0..40 {
             counter.decrement_irreversible_move();
         }
 
-        assert_eq!(counter.irreversible_move, vec![12, 0, 0, 231]);
+        assert_eq!(counter.irreversible_moves, vec![12, 0, 0, 231]);
 
         for _ in 0..240 {
             counter.decrement_irreversible_move();
         }
 
-        assert_eq!(counter.irreversible_move, vec![6]);
+        assert_eq!(counter.irreversible_moves, vec![6]);
 
         for _ in 0..1024 {
             counter.decrement_irreversible_move();
         }
-        assert_eq!(counter.irreversible_move, vec![0]);
+        assert_eq!(counter.irreversible_moves, vec![0]);
     }
 
     #[test]
     fn get_last_irreversible_move() {
         let counter = MoveCounter {
-            halfmove: 21,
-            irreversible_move: vec![0],
+            halfmoves: 21,
+            irreversible_moves: vec![0],
         };
         assert_eq!(counter.get_last_irreversible_move(), 0);
 
         let counter = MoveCounter {
-            halfmove: 21,
-            irreversible_move: vec![123, 34, 231, 0, 23],
+            halfmoves: 21,
+            irreversible_moves: vec![123, 34, 231, 0, 23],
         };
         assert_eq!(counter.get_last_irreversible_move(), 23);
     }
